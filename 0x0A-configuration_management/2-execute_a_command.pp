e4d21ac171f607a5fa91bde4efa06da8fd0572d4
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-09-22 22:09:07 +0300

    Execute pkill command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,6 @@
 # kills a process named killmenow
 
-
-exec { 'kill_killmenow':
-  command     => '/usr/bin/pkill killmenow',
-  refreshonly => true,
-  onlyif      => '/usr/bin/pgrep killmenow',
-  path        => '/usr/bin:/bin',
+exec { 'pkill killmenow':
+  path    => '/bin/',
+  command => 'pkill killmenow',
 }
